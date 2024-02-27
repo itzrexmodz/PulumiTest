28624737d818471b058f54645f804e19cc2cb94f
Адитья <78430010+itzrexmodz@users.noreply.github.com>
2024-02-27 15:33:40 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,28 +1,8 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
 import pulumi
-import pulumi_aws as aws
+from pulumi import Output
+from pulumi_cloud import Server
 
 size = "t2.micro"
-
-ami = aws.ec2.get_ami(
-    most_recent=True,
-    owners=["amazon"],
-    filters=[aws.ec2.GetAmiFilterArgs(name="name", values=["amzn2-ami-hvm-*"])],
-)
-
-group = aws.ec2.SecurityGroup(
-    "web-secgrp",
-    description="Enable HTTP access",
-    ingress=[
-        aws.ec2.SecurityGroupIngressArgs(
-            protocol="tcp",
-            from_port=80,
-            to_port=80,
-            cidr_blocks=["0.0.0.0/0"],
-        )
-    ],
-)
 
 user_data = """
 #!/bin/bash
@@ -33,12 +13,11 @@ pip3 install -r requirements.txt
 nohup python3 -m pyUltroid 80 &
 """
 
-server = aws.ec2.Instance(
-    "web-server-www",
-    instance_type=size,
-    vpc_security_group_ids=[group.id],
+server = Server(
+    'web-server-www',
+    opts=pulumi.ResourceOptions(depends_on=[]),
+    size=size,
     user_data=user_data,
-    ami=ami.id,
 )
 
 pulumi.export("public_ip", server.public_ip)
